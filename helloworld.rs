ffcea31deda2d7b4252d636eefacc6557bcc2b70
Alexandre Simaillaud <110174494+AlexandreSmd@users.noreply.github.com>
2024-04-01 14:23:23 +0200

    Add files via upload

---
+++ helloworld.rs
@@ -0,0 +1,12 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
+entrypoint!(hello_world);
+pub fn hello_world(
+    _program_id: &Pubkey, // Public key of the account the program was loaded into
+    accounts: &[AccountInfo], // All accounts required to process the instruction
+    _instruction_data: &[u8], // Serialized instruction-specific data
+) -> ProgramResult {
+    msg!("Hello {:}!!", accounts[0].key);
+    Ok(())
+}
